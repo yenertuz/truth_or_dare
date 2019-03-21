c3d34943ef41ebf736343cbbfce40f54a7ca7599
yenertuz <yenertuz@hotmail.com>
2019-03-20 23:29:38 -0700

    wed night

--- bundle.js
+++ bundle.js
@@ -251,6 +251,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var querystring__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! querystring */ "./node_modules/querystring-es3/index.js");
+/* harmony import */ var querystring__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(querystring__WEBPACK_IMPORTED_MODULE_2__);
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -271,6 +273,23 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 
 
+
+
+var is_room_name_okay = function is_room_name_okay(room_name) {
+  var allowed = "abcdefghijklmnopqrstuvwxyz0123456789";
+  var return_value = "";
+
+  if (room_name.length > 20) {
+    return "error";
+  }
+
+  room_name.split("").forEach(function (element) {
+    if (allowed.includes(element) == false) {
+      return_value = "error";
+    }
+  });
+  return return_value;
+};
 
 var MainOptions =
 /*#__PURE__*/
@@ -328,17 +347,65 @@ function (_React$Component2) {
   _createClass(MainCreate, [{
     key: "render",
     value: function render() {
+      if (state.span_class == undefined) {
+        state.span_class = "";
+      }
+
+      var room_name_taken = "";
+
+      if (state.room_name_taken == 1) {
+        room_name_taken = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+          className: "error",
+          id: "main-create-error-span"
+        }, "Room name already exists. Please choose another room name");
+      } else {
+        room_name_taken = "";
+      }
+
+      var create_button_onclick = "";
+
+      if (state.span_class != "error") {
+        create_button_onclick = function create_button_onclick() {
+          alert("CREATING");
+        };
+      }
+
       var go_back = function go_back() {
         state.action = "";
+        state.rerender();
+      };
+
+      var room_name_onchange = function room_name_onchange(e) {
+        e.preventDefault();
+        var room_name = e.target.value.toLowerCase();
+        e.target.value = room_name;
+        state.span_class = is_room_name_okay(room_name);
         state.rerender();
       };
 
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         id: "main-create-div",
         className: "container"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "Main Create"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
         onClick: go_back
-      }, "Go back"));
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+        className: "fas fa-arrow-left"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        id: "main-create-span"
+      }, "Enter a room name. ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        className: state.span_class
+      }, "Alphanumeric characters only, 20 character max"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), "Case insensitive. Max 20 people per room"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+        type: "text",
+        placeholder: "Example: cool42",
+        id: "main-create-input",
+        onChange: function onChange(e) {
+          room_name_onchange(e);
+        }
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        id: "main-create-button",
+        className: state.span_class,
+        onClick: create_button_onclick
+      }, "Create Room"), room_name_taken);
     }
   }]);
 
@@ -364,12 +431,53 @@ function (_React$Component3) {
         state.rerender();
       };
 
+      if (state.span_class == undefined) {
+        state.span_class = "";
+      }
+
+      var join_button_onclick = "";
+
+      if (state.span_class != "error") {
+        join_button_onclick = function join_button_onclick() {
+          state.target_room = state.temp;
+          state.temp = "";
+          state.rerender();
+        };
+      } else {
+        join_button_onclick = function join_button_onclick() {};
+      }
+
+      var room_name_onchange = function room_name_onchange(e) {
+        e.preventDefault();
+        state.temp = e.target.value.toLowerCase();
+        e.target.value = state.temp;
+        state.span_class = is_room_name_okay(state.temp);
+        state.rerender();
+      };
+
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         id: "main-join-div",
         className: "container"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "Main Join"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        id: "main-join-button",
         onClick: go_back
-      }, "Go back"));
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+        className: "fas fa-arrow-left"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        id: "main-join-span"
+      }, "Enter a room name to join. ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        className: state.span_class
+      }, "Room names are alphanumeric, lowercase, max 20 characters.")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+        id: "main-join-input",
+        placeholder: "Example: fun42",
+        onChange: function onChange(e) {
+          room_name_onchange(e);
+        }
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        className: state.span_class,
+        id: "main-join-button-2",
+        onClick: join_button_onclick
+      }, "Join"));
     }
   }]);
 
@@ -737,6 +845,215 @@ module.exports = checkPropTypes;
 var ReactPropTypesSecret = 'SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED';
 
 module.exports = ReactPropTypesSecret;
+
+
+/***/ }),
+
+/***/ "./node_modules/querystring-es3/decode.js":
+/*!************************************************!*\
+  !*** ./node_modules/querystring-es3/decode.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+// If obj.hasOwnProperty has been overridden, then calling
+// obj.hasOwnProperty(prop) will break.
+// See: https://github.com/joyent/node/issues/1707
+function hasOwnProperty(obj, prop) {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+module.exports = function(qs, sep, eq, options) {
+  sep = sep || '&';
+  eq = eq || '=';
+  var obj = {};
+
+  if (typeof qs !== 'string' || qs.length === 0) {
+    return obj;
+  }
+
+  var regexp = /\+/g;
+  qs = qs.split(sep);
+
+  var maxKeys = 1000;
+  if (options && typeof options.maxKeys === 'number') {
+    maxKeys = options.maxKeys;
+  }
+
+  var len = qs.length;
+  // maxKeys <= 0 means that we should not limit keys count
+  if (maxKeys > 0 && len > maxKeys) {
+    len = maxKeys;
+  }
+
+  for (var i = 0; i < len; ++i) {
+    var x = qs[i].replace(regexp, '%20'),
+        idx = x.indexOf(eq),
+        kstr, vstr, k, v;
+
+    if (idx >= 0) {
+      kstr = x.substr(0, idx);
+      vstr = x.substr(idx + 1);
+    } else {
+      kstr = x;
+      vstr = '';
+    }
+
+    k = decodeURIComponent(kstr);
+    v = decodeURIComponent(vstr);
+
+    if (!hasOwnProperty(obj, k)) {
+      obj[k] = v;
+    } else if (isArray(obj[k])) {
+      obj[k].push(v);
+    } else {
+      obj[k] = [obj[k], v];
+    }
+  }
+
+  return obj;
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+
+/***/ }),
+
+/***/ "./node_modules/querystring-es3/encode.js":
+/*!************************************************!*\
+  !*** ./node_modules/querystring-es3/encode.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+var stringifyPrimitive = function(v) {
+  switch (typeof v) {
+    case 'string':
+      return v;
+
+    case 'boolean':
+      return v ? 'true' : 'false';
+
+    case 'number':
+      return isFinite(v) ? v : '';
+
+    default:
+      return '';
+  }
+};
+
+module.exports = function(obj, sep, eq, name) {
+  sep = sep || '&';
+  eq = eq || '=';
+  if (obj === null) {
+    obj = undefined;
+  }
+
+  if (typeof obj === 'object') {
+    return map(objectKeys(obj), function(k) {
+      var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+      if (isArray(obj[k])) {
+        return map(obj[k], function(v) {
+          return ks + encodeURIComponent(stringifyPrimitive(v));
+        }).join(sep);
+      } else {
+        return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+      }
+    }).join(sep);
+
+  }
+
+  if (!name) return '';
+  return encodeURIComponent(stringifyPrimitive(name)) + eq +
+         encodeURIComponent(stringifyPrimitive(obj));
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+function map (xs, f) {
+  if (xs.map) return xs.map(f);
+  var res = [];
+  for (var i = 0; i < xs.length; i++) {
+    res.push(f(xs[i], i));
+  }
+  return res;
+}
+
+var objectKeys = Object.keys || function (obj) {
+  var res = [];
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) res.push(key);
+  }
+  return res;
+};
+
+
+/***/ }),
+
+/***/ "./node_modules/querystring-es3/index.js":
+/*!***********************************************!*\
+  !*** ./node_modules/querystring-es3/index.js ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+exports.decode = exports.parse = __webpack_require__(/*! ./decode */ "./node_modules/querystring-es3/decode.js");
+exports.encode = exports.stringify = __webpack_require__(/*! ./encode */ "./node_modules/querystring-es3/encode.js");
 
 
 /***/ }),
--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { stringify } from 'querystring';
+
+var is_room_name_okay = (room_name) => {
+	let allowed = "abcdefghijklmnopqrstuvwxyz0123456789";
+	let return_value = "";
+
+	if (room_name.length > 20) {
+		return "error";
+	}
+	room_name.split("").forEach(
+		(element) => {
+			if (allowed.includes(element) == false) {
+				return_value = "error";
+			}
+		}
+	);
+	return return_value;
+}
 
 class MainOptions extends React.Component {
     render () {
@@ -24,17 +42,46 @@ class MainOptions extends React.Component {
 
 class MainCreate extends React.Component {
     render() {
+		if (state.span_class == undefined) {
+			state.span_class = "";
+		}
+
+		let room_name_taken = "";
+		if (state.room_name_taken == 1) {
+			room_name_taken = <span className="error" id="main-create-error-span">
+			Room name already exists. Please choose another room name</span>;
+		} else {
+			room_name_taken = "";
+		}
+
+		let create_button_onclick = "";
+		if (state.span_class != "error") {
+			create_button_onclick = () => {
+				alert("CREATING");
+			}
+		}
+
         let go_back = () => {
             state.action = "";
             state.rerender();
-        }
+		}
+		
+		let room_name_onchange = (e) => {
+			e.preventDefault();
+			let room_name = e.target.value.toLowerCase();
+			e.target.value = room_name;
+			state.span_class = is_room_name_okay(room_name);
+			state.rerender();
+		}
 
         return (
-
             <div id="main-create-div" className="container">
-            <span>Main Create</span>
-            <button onClick={go_back}>Go back</button>
-            </div>
+            <button onClick={go_back}><i className="fas fa-arrow-left"></i></button>
+			<span id="main-create-span">Enter a room name. <span className={state.span_class}>Alphanumeric characters only, 20 character max</span><br />Case insensitive. Max 20 people per room</span>
+			<input type="text" placeholder="Example: cool42" id="main-create-input" onChange={(e) => {room_name_onchange(e);}}/>
+			<button id="main-create-button" className={state.span_class} onClick={create_button_onclick}>Create Room</button>
+            {room_name_taken}
+			</div>
         );
     }
 }
@@ -44,12 +91,38 @@ class MainJoin extends React.Component {
         let go_back = () => {
             state.action = "";
             state.rerender();
-        }
+		}
+
+		if (state.span_class == undefined) {
+			state.span_class = "";
+		}
+		let join_button_onclick = "";
+		if (state.span_class != "error") {
+			join_button_onclick = () => {
+				state.target_room = state.temp;
+				state.temp = "";
+				state.rerender();
+			};
+		} else {
+			join_button_onclick = () => {};
+		}
+		
+		let room_name_onchange = (e) => {
+			e.preventDefault();
+			state.temp = e.target.value.toLowerCase();
+			e.target.value = state.temp;
+			state.span_class = is_room_name_okay(state.temp);
+			state.rerender();
+		}
 
         return (
             <div id="main-join-div" className="container">
-            <span>Main Join</span>
-            <button onClick={go_back}>Go back</button>
+            <button id="main-join-button" onClick={go_back}><i className="fas fa-arrow-left"></i></button>
+			<span id="main-join-span">Enter a room name to join. <br />
+			<span className={state.span_class}>Room names are alphanumeric, lowercase, max 20 characters.</span></span>
+			<input id="main-join-input" placeholder="Example: fun42" onChange={(e) => {room_name_onchange(e);}} />
+			<button className={state.span_class} id="main-join-button-2"
+			onClick={join_button_onclick} >Join</button>
             </div>
         );
     }
